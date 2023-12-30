db20b26876adb3fe629716c34c3427ddb87425ad
t2techno <ttuschhoff11@gmail.com>
2023-12-30 15:10:13 -0600

    using css variables instead

--- src/components/GlobalStyles/GlobalStyles.js
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -76,6 +76,23 @@ html {
 html, body, #root {
   height: 100%;
 }
+
+/* VARIABLES */
+:root {
+  --color-primary: hsl(340deg 65% 47%);
+  --color-secondary: hsl(240deg 60% 63%);
+  --color-white: hsl(0deg 0% 100%);
+
+  --color-gray-100: hsl(185deg 5% 95%);
+  --color-gray-300: hsl(190deg 5% 80%);
+  --color-gray-500: hsl(196deg 4% 60%);
+  --color-gray-700: hsl(220deg 5% 40%);
+  --color-gray-900: hsl(220deg 3% 20%);
+
+  --font-weight-normal: 500;
+  --font-weight-medium: 600;
+  --font-weight-bold: 800;
+}
 `;
 
 export default GlobalStyles;
